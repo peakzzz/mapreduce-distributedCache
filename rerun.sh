b47d4f7f77284a1749dfc547857d2656be4fa0ba
Preeti Krishnan <peakzzz@gmail.com>
2016-11-19 18:38:04 -0800

    Update rerun.sh

--- rerun.sh
+++ rerun.sh
@@ -3,6 +3,5 @@
 export CLASSPATH=$(hadoop classpath)
 export HADOOP_CLASSPATH=$CLASSPATH
 
-rm -rf /user/user01/LAB1/E2/OUT
-hadoop jar DCacheJoin.jar DCacheJoin.DCacheJoinDictionaryDriver -files /user/user01/LAB1/E2/DATA/latin.txt /user/user01/LAB1/E1/OUT/part-r-00000 /user/user01/LAB1/E2/OUT 
-
+rm -rf /user/user01/OUT_LATIN
+hadoop jar DCacheJoin.jar DCacheJoin.DCacheJoinDictionaryDriver -files /user/user01/INPUT_LATIN/latin.txt /user/user01/OUT/part-r-00000 /user/user01/OUT_LATIN
