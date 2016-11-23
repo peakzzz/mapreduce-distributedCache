b259f8004605b479feb8d735f93dacdb1298efeb
Preeti Krishnan <peakzzz@gmail.com>
2016-11-22 21:31:41 -0800

    Update DCacheDictionaryReducer.java

--- DCacheDictionaryReducer.java
+++ DCacheDictionaryReducer.java
@@ -8,7 +8,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class DCacheJoinDictionaryReducer extends Reducer<Text,Text,Text,Text> {
       public void reduce(Text word, Iterable<Text> values, Context context ) throws IOException, InterruptedException {
-	// TODO: add appropriate translations to the existing word/part-of-speech translations
+	// appropriate translations to the existing word/part-of-speech translations
       }
 
    }
